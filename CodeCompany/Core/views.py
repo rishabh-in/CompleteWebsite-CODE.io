769b0a7d90055a1cf195567ba0ddf5567757ad29
rishabh-in <43580878+rishabh-in@users.noreply.github.com>
2020-01-26 20:10:39 +0530

    Update views.py

--- CodeCompany/Core/views.py
+++ CodeCompany/Core/views.py
@@ -8,7 +8,7 @@ core=Blueprint("core",__name__)
 @core.route("/",methods=["GET","POST"])
 def index():
     page=request.args.get("page",1,type=int)
-    code_post=CodePost.query.order_by(CodePost.date.desc()).paginate(page=page, per_page=6)
+    code_post=CodePost.query.order_by(CodePost.date.desc()).paginate(page=page, per_page=8)
     return render_template("home.html",code_post=code_post)
 
 @core.route("/info")
